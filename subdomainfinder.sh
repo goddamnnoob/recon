db1e360a7e2e88d2e6191e74b9ee33dbdae2bcd1
Gowtham <48803263+goddamnnoob@users.noreply.github.com>
2020-10-02 13:06:20 +0530

    fixed data mismatch

--- subdomainfinder.sh
+++ subdomainfinder.sh
@@ -6,13 +6,15 @@ touch $output
 echo "SUBDOMAIN ENUMERATION"|tee -a $output
 echo "-----------LOL---------------"
 echo "------------sublist3r tool---------"
-sublist3r -d $target -o $output 
+sublist3r -d $target -o s1.txt 
 echo "--------------subfinder tool-----------"
-subfinder -d $target -o $output 
+subfinder -d $target -o s2.txt 
 echo "------------knockpy tool-------------"
 knockpy  $target | tee -a $output 
 echo "-----------dnsrecon tool----------"
-dnsrecon -n 8.8.8.8 -d $target| tee -a $output 
+dnsrecon -n 8.8.8.8 -d $target| tee -a $output
+cat s1.txt s2.txt > $output
+rm s1.txt && rm s2.txt
 sort $output | uniq -u
 echo "completed scaning..........:) "
 cat $output
